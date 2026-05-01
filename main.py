25d0b1bf7ea2efa2c1caa507021a5d2275297163
Nizigiymana-Tech <junkacc830@gmail.com>
2026-05-01 11:35:20 -0400

    Added add

--- main.py
+++ main.py
@@ -7,13 +7,47 @@
 # success message
 # if "show"
 #print out list
-from term_image.image import from_file
+import climage
 import time
 
 Books = []
-image = from_file("book-nook-logo.png")
+Options = ["add", "remove", "count", "q"]
+image = climage.convert("images/book-nook-logo.png", is_unicode=True, width=80)
 
 # Welcome Message
 print("Welcome to...")
 time.sleep(2)
-image.draw()
+print(image)
+print("""""
+         ______ ______
+        _/      Y      \_
+       // ~Book | ~~ ~  \
+      // ~ ~ ~~ |  Nook~ \      
+     //________.|.________\     
+    `----------`-'----------'
+    """)
+
+def add():
+    BookName = input("What's the book's name? ")
+    Books.append(BookName)
+    print("Added your book!")
+    print(f"Books: {Books}")
+    return
+
+#Open Menu
+while True:
+    print("""
+        Menu: 
+        Add book (add)
+        Remove book (remove) 
+        Show inventory count (count) 
+        Quit (q)
+        """)
+    
+    Action = input("What would you like to do? ")
+
+    try:
+        Action()
+    except TypeError:
+        print("Not a option. Choose again!")
+        
